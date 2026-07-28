eabf2fc8d8a8e2a1eb6f663d109b2042bd328c2b
chirrayu <chirrayu.sharma@gmail.com>
2026-07-29 01:41:54 +0530

    fix

--- software/frontend/app/src/main/java/com/example/healthmonitor/NetworkConfig.kt
+++ software/frontend/app/src/main/java/com/example/healthmonitor/NetworkConfig.kt
@@ -1,5 +1,5 @@
 package com.example.healthmonitor
-
+// change made
 object NetworkConfig {
     const val BASE_URL = "https://triage-ai-backend-v13x.onrender.com"
     const val WS_URL   = "wss://triage-ai-backend-v13x.onrender.com/ws/connect?feed=all"
